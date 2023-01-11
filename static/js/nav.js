522062bbf07f86f6e4fd0cdd12c59541d40182a4
D~Evil NightRaid <evil77077077@gmail.com>
2023-01-11 03:07:39 -0500

    50%

---
+++ static/js/nav.js
@@ -0,0 +1,17 @@
+var hamburger = document.getElementById('hame');
+var mainCon = document.getElementById('mainContainer');
+var modata = document.getElementById('mdata');
+var foot = document.getElementById('foot')
+function showHide() {
+    if (mainCon.style.display === "flex") {
+        mainCon.style.display = 'none';
+        modata.style.display = "flex";
+        hamburger.src ='../static/imgs/icons/close-icon.svg';
+        foot.style.display = 'none';
+    } else {
+        mainCon.style.display = "flex";
+        modata.style.display = "none";
+        hamburger.src ='../static/imgs/icons/hamburger.svg'
+        foot.style.display = 'flex';
+    }
+}
